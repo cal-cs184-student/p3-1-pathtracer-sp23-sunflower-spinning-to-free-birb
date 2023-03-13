6220df541666d2538805ead8b0a8bcac00385f6f
Ziqi Shi <ziqi.shi@berkeley.edu>
2023-03-13 13:04:21 -0700

    Buildable

--- src/scene/triangle.cpp
+++ src/scene/triangle.cpp
@@ -27,7 +27,18 @@ bool Triangle::has_intersection(const Ray &r) const {
   // The difference between this function and the next function is that the next
   // function records the "intersection" while this function only tests whether
   // there is a intersection.
-
+	Vector3D e1(p2 - p1), e2(p3 - p1);
+	Vector3D n(cross(e1, e2));
+	if (abs(dot(n, r.d) - 0.0) <= 0.000001) return false;
+	Matrix3x3 mat(
+		-r.d.x, e1.x, e2.x,
+		-r.d.y, e1.y, e2.y,
+		-r.d.z, e1.z, e2.z);
+	Vector3D tuv(mat.inv() * (r.o - p1));
+	float t(tuv.x), u(tuv.y), v(tuv.z), w(1.0 - u - v);
+	if (!((u >= 0.0) && (v >= 0.0) && (w >= 0.0))) return false;
+	if (!((t >= r.min_t) && (t <= r.max_t))) return false;
+	r.max_t = t;
 
   return true;
 
@@ -39,13 +50,20 @@ bool Triangle::intersect(const Ray &r, Intersection *isect) const {
   // place, the Intersection data should be updated accordingly
 	Vector3D e1(p2 - p1), e2(p3 - p1);
 	Vector3D n(cross(e1, e2));
-	if (dot(n, r.d) == 0) return false;
+	if (abs(dot(n, r.d) - 0.0) <= 0.000001) return false;
 	Matrix3x3 mat(
 		-r.d.x, e1.x, e2.x,
 		-r.d.y, e1.y, e2.y,
 		-r.d.z, e1.z, e2.z);
 	Vector3D tuv(mat.inv() * (r.o - p1));
-	if(!())
+	float t(tuv.x), u(tuv.y), v(tuv.z), w(1.0 - u - v);
+	if (!((u >= 0.0) && (v >= 0.0) && (w >= 0.0))) return false;
+	if (!((t >= r.min_t) && (t <= r.max_t))) return false;
+	r.max_t = t;
+	isect->t = t;
+	isect->n = n1*w + n2*u + n3*v;
+	isect->primitive = this;
+	isect->bsdf = get_bsdf();
 
   return true;
 
